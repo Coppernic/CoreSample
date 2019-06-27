73a9a7b3c0d4261620e0bf784b8e5f3d83ad72b1
Ash <clement.coxam@coppernic.fr>
2019-06-27 19:00:18 +0200

    [DEV] Add NetPresenter

--- app/src/main/java/fr/coppernic/samples/core/ui/NetPresenter.kt
+++ app/src/main/java/fr/coppernic/samples/core/ui/NetPresenter.kt
@@ -3,14 +3,29 @@ package fr.coppernic.samples.core.ui
 class NetPresenter () {
 
 
-    fun isValidIp () {
+    fun isValidIp () : Boolean {
 
-    }
-    fun isValidMask () {
-
-    }
-    fun fromMasktoPrefix () {
-
+        val regexIp = android.util.Patterns.IP_ADDRESS
+        return isValidIp()
     }
 
+    fun isValidMask ():Boolean {
+
+        val regexMask = Regex(pattern = "([1-9]|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])(\\.(\\d|[1-9]\\d|1\\d{2}|2[0-4]\\d|25[0-5])){3}\\/\\d+")
+        return isValidMask()
+    }
+    fun fromMasktoPrefix(netmask: Int):Int {
+
+        return Integer.bitCount(netmask)
+
+    }
+    @Throws(IllegalArgumentException::class)
+
+    fun prefixLengthToNetmaskInt(prefixLength: Int): Int {
+        if (prefixLength < 0 || prefixLength > 32) {
+            throw IllegalArgumentException("Invalid prefix length (0 <= prefix <= 32)")
+        }
+        val value = -0x1 shl 32 - prefixLength //ou val value = -0x1 shl 32 - prefixLength
+        return Integer.reverseBytes(value)
+    }
 }
