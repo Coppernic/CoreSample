fe191033e1eca02f1234e3c8a66ef0bcf5f8d831
Ash <clement.coxam@coppernic.fr>
2019-10-09 10:18:47 +0200

    [DEV] Change deprecated methods

--- app/src/main/java/fr/coppernic/samples/core/MainActivity.kt
+++ app/src/main/java/fr/coppernic/samples/core/MainActivity.kt
@@ -8,7 +8,6 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import fr.coppernic.samples.core.ui.*
-import fr.coppernic.sdk.utils.helpers.CpcOs
 import fr.coppernic.sdk.utils.helpers.OsHelper
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
