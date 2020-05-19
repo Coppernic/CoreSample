a43c39dc3e32a87dc1c4b8db1be3128a2f9ff048
Jean-François Poughon <jean-francois.poughon@coppernic.fr>
2020-05-19 14:20:24 +0200

    Applying code review change

--- app/src/main/java/fr/coppernic/samples/core/ui/ApiMappingFragment.kt
+++ app/src/main/java/fr/coppernic/samples/core/ui/ApiMappingFragment.kt
@@ -55,7 +55,7 @@ class ApiMappingFragment : androidx.fragment.app.Fragment() {
 
     private fun loadFromMapper(progKey: Mapper.ProgKey) {
         val type = mapper.getMappingType(progKey)
-        var actionName = "";
+        var actionName = ""
 
         when (type) {
             Mapper.MappingType.KEY -> actionName = loadKey(mapper.getKeyMapping(progKey))
