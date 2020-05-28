170b8f6c80b65b202561e6b107ea7f80e0cc9b38
Jean-François Poughon <jean-francois.poughon@coppernic.fr>
2020-05-28 15:43:44 +0200

    wip

--- app/src/main/java/fr/coppernic/samples/core/ui/hardwareTools/HdwrTestCone.kt
+++ app/src/main/java/fr/coppernic/samples/core/ui/hardwareTools/HdwrTestCone.kt
@@ -1,92 +1,84 @@
 package fr.coppernic.samples.core.ui.hardwareTools
 
-import android.content.BroadcastReceiver
 import android.content.Context
-import android.content.Intent
+import android.os.SystemClock
 import android.util.Log
 import androidx.fragment.app.FragmentManager
 import fr.coppernic.sdk.hdk.cone.GpioPort
 import fr.coppernic.sdk.serial.SerialCom
 import fr.coppernic.sdk.serial.SerialFactory
+import fr.coppernic.sdk.utils.core.CpcBytes
 import fr.coppernic.sdk.utils.core.CpcResult.RESULT
 import fr.coppernic.sdk.utils.io.InstanceListener
+import fr.coppernic.sdk.utils.io.Interruptables
+import fr.coppernic.sdk.utils.usb.UsbHelper
 import io.reactivex.Completable
+import io.reactivex.SingleObserver
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import timber.log.Timber
 import java.util.*
 import java.util.concurrent.Semaphore
+import java.util.concurrent.TimeUnit
 
+private const val TAG = "ExpansionPortTest"
+
+private const val SERIAL_PORT = "/dev/ttyHSL1"
+private const val FTX230X_VID = 1027
+private const val FTX230X_PID = 24597
 
 class ExpansionPortTest(ctx: Context?, man: FragmentManager,
                         notifier: HdwrTestResultInterface?) : HdwrTestDialogBase(ctx, man, notifier) {
-    override val tag = "ExpansionPortTest"
-    private val SERIAL_PORT = "/dev/ttyHSL1"
-    private val context = ctx
+
+
     private val semUsb = Semaphore(0)
+
     private lateinit var directSerial: SerialCom
     private lateinit var ftdiSerial: SerialCom
     private lateinit var gpioPort: GpioPort
 
-    private val mUsbDevicePermissions: BroadcastReceiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val action = intent.action
-//                if (FtdiSerialCommunication.USB_INTENT_FILTER.equals(action)) {
-//                    val device = intent
-//                            .getParcelableExtra<Parcelable>("device") as UsbDevice
+//        private BroadcastReceiver mUsbDevicePermissions = new BroadcastReceiver() {
+//            public void onReceive(Context context, Intent intent) {
+//                String action = intent.getAction();
+//                if (UsbHelper.ACTION_USB_PERMISSION.equals(action)) {
+//                    UsbDevice device = intent
+//                            .getParcelableExtra("device");
 //                    if (!intent.getBooleanExtra("permission", false)) {
-//                        Log.d(tag, "permission denied for device "
-//                                + device)
-//                        postError("[FAILED] : Permission denied for USB device")
+//                        Timber.d("permission denied for device %s", device);
+//                        postError("[FAILED] : Permission denied for USB device");
 //                    } else {
-//                        mResult = RESULT.OK
+//                        mResult = RESULT.OK;
 //                    }
-//                    semUsb.release()
+//                    semUsb.release();
 //                }
-        }
-    }
-
-    @Throws(Exception::class)
-    override fun startTest(): RESULT {
-        Timber.d("Test Gpio 3")
-        gpioPort.setPin3(false)
-        isGpio3Down
-        gpioPort.setPin3(true)
-
-//            // semUsb.acquire();
-//            if (false == semUsb.tryAcquire(10, TimeUnit.SECONDS)) {
-//                postErrorAndFail("Usb was not activated on time")
 //            }
-//            // Return error is no permission to access to FTDI
-//            if (mResult === RESULT.ERROR) {
-//                // postError("[FAIL] Test 1 : Output 3 is LOW or USB is unavailable");
-//                postErrorAndFail("[FAIL] Test 1 : Output 3 is LOW or USB is unavailable")
-//            }
-        isGpio3Up
-        Timber.d("Test Com")
-        isComOk.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
+//        };
 
-                }, {
-                    waitOnError(it.message)
-                }
-                )
+    //        private BroadcastReceiver mUsbDevicePermissions = new BroadcastReceiver() {
+    //            public void onReceive(Context context, Intent intent) {
+    //                String action = intent.getAction();
+    //                if (UsbHelper.ACTION_USB_PERMISSION.equals(action)) {
+    //                    UsbDevice device = intent
+    //                            .getParcelableExtra("device");
+    //                    if (!intent.getBooleanExtra("permission", false)) {
+    //                        Timber.d("permission denied for device %s", device);
+    //                        postError("[FAILED] : Permission denied for USB device");
+    //                    } else {
+    //                        mResult = RESULT.OK;
+    //                    }
+    //                    semUsb.release();
+    //                }
+    //            }
+    //        };
 
-
-        Log.d(tag, "Test Gpio 1 et 2")
-        areGpio1and2Ok()
-        Log.d(tag, "Test Input")
-        isInput4OK
-        return mResult
-    }
 
     override fun setUp(): Completable? {
-        Log.d(tag, "setUp")
+        Timber.d("setUp")
         super.setUp()
         return Completable.create { emitter ->
             val g = GpioPort.GpioManager.get()
-                    .getGpioSingle(context)
+                    .getGpioSingle(mCtx)
                     .observeOn(Schedulers.io())
                     .subscribe({
                         gpioPort = it
@@ -97,7 +89,7 @@ class ExpansionPortTest(ctx: Context?, man: FragmentManager,
                     })
         }.andThen(
                 Completable.create { emit ->
-                    val d = context?.let {
+                    val d = mCtx?.let {
                         SerialFactory.getInstance(it, SerialCom.Type.DIRECT, object : InstanceListener<SerialCom> {
                             override fun onDisposed(p0: SerialCom?) {
 
@@ -113,7 +105,7 @@ class ExpansionPortTest(ctx: Context?, man: FragmentManager,
                 }
         ).andThen(
                 Completable.create { emit ->
-                    val f = context?.let {
+                    val f = mCtx?.let {
                         SerialFactory.getInstance(it, SerialCom.Type.FTDI, object : InstanceListener<SerialCom> {
                             override fun onDisposed(p0: SerialCom?) {
 
@@ -148,14 +140,32 @@ class ExpansionPortTest(ctx: Context?, man: FragmentManager,
             // Bug case where several permission windows are shown
 //            while (true == semUsb.tryAcquire()) {
 //                // acquire all sem available
-//            }
+//           }
         }
+        Timber.d("setup end")
+    }
+
+    @Throws(Exception::class)
+    override fun startTest(): RESULT? {
+        Log.d(TAG, "Test Gpio 3")
+        gpioPort.setPin3(false)
+        isGpio3Down()
+        gpioPort.setPin3(true)
+        isGpio3Up()
+        Timber.d("Test Com")
+        isComOk()
+        Timber.d("Test Gpio 1 et 2")
+        areGpio1and2Ok()
+        Timber.d("Test Input")
+        isInput4OK()
+        return mResult
     }
 
     override fun tearDown() {
-        Log.d(tag, "tearDown")
-        context?.applicationContext?.unregisterReceiver(
-                mUsbDevicePermissions)
+        Timber.d("tearDown")
+
+//            mCtx.getApplicationContext().unregisterReceiver(
+//                    mUsbDevicePermissions);
         if (directSerial.isOpened) {
             directSerial.close()
         }
@@ -167,182 +177,131 @@ class ExpansionPortTest(ctx: Context?, man: FragmentManager,
     }
 
     private fun resetGpio() {
+        Log.d(TAG, "resetGpio")
         gpioPort.setPin1(false)
         gpioPort.setPin2(false)
         gpioPort.setPin3(false)
         gpioPort.setPin4(false)
+        gpioPort.setPinEn(false)
+        gpioPort.setPinUsbEn(false)
+        gpioPort.setPinUsbIdSw(false)
     }
 
-    @get:Throws(Exception::class)
-    private val isGpio3Down: Boolean
-        private get() {
-            var ret = true
-            val devices: Array<String> = ftdiSerial.listDevices()
-            if (devices != null && devices.isNotEmpty()) {
-                Log.e(tag, "Ftdi is on")
-                Timber.e("[FAIL] Test 1 : Gpio 3 is up instead of down")
-                waitOnError("[FAIL] Test 1 : Gpio 3 is up instead of down")
-                ret = false
-            }
-            return ret
+    @Throws(Exception::class)
+    private fun isGpio3Down() {
+        Log.d(TAG, "isGpio3Down")
+        val devices = ftdiSerial.listDevices()
+        if (devices != null && devices.isNotEmpty()) {
+            Log.e(TAG, "Ftdi is on")
+            Timber.e("Ftdi is on")
+            waitOnError("[FAIL] Test 1 : Gpio 3 is up instead of down")
         }
+    }
 
-    // postError("[FAIL] Test 1 : Gpio 3 is down instead of up");
-    @get:Throws(TestFailException::class)
-    private val isGpio3Up: Boolean
-        private get() {
-            var ret = true
-            val devices: Array<String> = ftdiSerial.listDevices()
-            if (devices.isEmpty()) {
-                Log.e(tag, "Ftdi is off")
-                //postErrorAndFail("[FAIL] Test 1 : Gpio 3 is down instead of up")
-                Timber.e("[FAIL] Test 1 : Gpio 3 is down instead of up")
-                postError("[FAIL] Test 1 : Gpio 3 is down instead of up");
-                ret = false
+    @Throws(TestFailException::class)
+    private fun isGpio3Up() {
+        Log.d(TAG, "isGpio3Up")
+        gpioPort.setPinEn(true)
+        gpioPort.setPinUsbEn(true)
+        gpioPort.setPinUsbIdSw(true)
+        val deviceConnected = UsbHelper.waitingForDeviceConnected(mCtx, FTX230X_VID, FTX230X_PID, 3, TimeUnit.SECONDS)
+        Log.d(TAG, "usb device " + UsbHelper.getUsbDeviceListLog(mCtx))
+        when (deviceConnected) {
+            RESULT.OK -> {
+                SystemClock.sleep(1200)
+                val devices = ftdiSerial.listDevices()
+                if (devices == null || devices.isEmpty()) {
+                    Log.e(TAG, "Ftdi is off")
+                    Timber.e("Ftdi is off")
+                    postErrorAndFail("[FAIL] Test 1 : Gpio 3 is down instead of up")
+                }
             }
-            return ret
+            else -> postErrorAndFail("Usb was not activated on time")
         }
+    }
 
-//    // Get device port
-//    @get:Throws(Exception::class)
-//    private val isComOk:
-//
-//    // clear buffer
-//            Boolean
-//        private get() {
-//            val ret = true
-//            val bufferOut = ByteArray(32)
-//            val bufferIn = ByteArray(32)
-//            populateBuffer(bufferOut)
-//            // Get device port
-//            val devices: Array<String> = ftdiSerial.listDevices()
-//            if (!devices.isNullOrEmpty()) {
-//                ftdiSerial.open(devices[0], 115200)
-//            }
-//            if (!ftdiSerial.isOpened || !directSerial.isOpened) {
-//                Timber.e("[FAIL] Test 2 : Port com and Ftdi are not opened")
-//                waitOnError("[FAIL] Test 2 : Port com and Ftdi are not opened")
-//            }
-//            Log.d(tag, "Com are opened")
-//
-//            // clear buffer
-//            directSerial.flush()
-//            ftdiSerial.send(bufferOut, bufferOut.size)
-//            ftdiSerial.flush()
-//            directSerial.receive(200, bufferIn.size, bufferIn)
-//            Log.d(tag, "compare")
-//            if (!bufferIn.contentEquals(bufferOut)) {
-//                Timber.e("[FAIL] Test 2 : Com from FTDI to Serial went wrong")
-//                waitOnError("[FAIL] Test 2 : Com from FTDI to Serial went wrong")
-//            }
-//            populateBuffer(bufferOut)
-//            Log.d(tag, "Second test")
-//            ftdiSerial.flush()
-//            directSerial.send(bufferOut, bufferOut.size)
-//            directSerial.flush()
-//            ftdiSerial.receive(200, bufferIn.size, bufferIn)
-//            if (!bufferIn.contentEquals(bufferOut)) {
-//                Timber.e("[FAIL] Test 2 : Com from Serial to FTDI went wrong")
-//                waitOnError("[FAIL] Test 2 : Com from Serial to FTDI went wrong")
-//            }
-//            Log.d(tag, " test end")
-//            return ret
-//        }
-
-    // Get device port
-    @get:Throws(Exception::class)
-    private val isComOk:
-
-    // clear buffer
-            Completable
-        private get() {
-            return Completable.create { emitter ->
-                val ret = true
-                val bufferOut = ByteArray(32)
-                val bufferIn = ByteArray(32)
-                populateBuffer(bufferOut)
-                // Get device port
-                val devices: Array<String> = ftdiSerial.listDevices()
-                if (!devices.isNullOrEmpty()) {
-                    ftdiSerial.open(devices[0], 115200)
-                }
-                if (!ftdiSerial.isOpened || !directSerial.isOpened) {
-                    Timber.e("[FAIL] Test 2 : Port com and Ftdi are not opened")
-//                    emitter.onError(Exception("[FAIL] Test 2 : Port com and Ftdi are not opened"))
-                    waitOnError("[FAIL] Test 2 : Port com and Ftdi are not opened")
-                }
-                Log.d(tag, "Com are opened")
-
-                // clear buffer
-                directSerial.flush()
-                ftdiSerial.send(bufferOut, bufferOut.size)
-                ftdiSerial.flush()
-                directSerial.receive(200, bufferIn.size, bufferIn)
-                Log.d(tag, "compare")
-                if (!bufferIn.contentEquals(bufferOut)) {
-                    Timber.e("[FAIL] Test 2 : Com from FTDI to Serial went wrong")
-                    waitOnError("[FAIL] Test 2 : Com from FTDI to Serial went wrong")
-                }
-                populateBuffer(bufferOut)
-                Log.d(tag, "Second test")
-                ftdiSerial.flush()
-                directSerial.send(bufferOut, bufferOut.size)
-                directSerial.flush()
-                ftdiSerial.receive(200, bufferIn.size, bufferIn)
-                if (!bufferIn.contentEquals(bufferOut)) {
-                    Timber.e("[FAIL] Test 2 : Com from Serial to FTDI went wrong")
-                    waitOnError("[FAIL] Test 2 : Com from Serial to FTDI went wrong")
-                }
-                Log.d(tag, " test end")
-                emitter.onComplete()
-            }
+    @Throws(Exception::class)
+    private fun isComOk() {
+        Log.d(TAG, "isComOk")
+        val bufferOut = ByteArray(32)
+        val bufferIn = ByteArray(32)
+        populateBuffer(bufferOut)
+        // Get device port
+        val devices = ftdiSerial.listDevices()
+        if (devices != null) {
+            ftdiSerial.open(devices[0], 9600)
         }
+        if (!ftdiSerial.isOpened || !directSerial.isOpened) {
+            waitOnError("[FAIL] Test 2 : Port com and Ftdi are not opened")
+        }
+        Timber.d("Com are opened")
+
+        // clear buffer
+        directSerial.flush()
+        ftdiSerial.send(bufferOut, bufferOut.size)
+        Log.d(TAG, "items length send " + CpcBytes.byteArrayToString(bufferOut))
+        directSerial.receive(200, bufferIn.size, bufferIn)
+        Log.d(TAG, "items length receive " + CpcBytes.byteArrayToString(bufferIn))
+        Timber.d("compare")
+        if (!Arrays.equals(bufferIn, bufferOut)) {
+            waitOnError("[FAIL] Test 2 : Com from FTDI to Serial went wrong")
+        }
+        populateBuffer(bufferOut)
+        Timber.d("Second test")
+        ftdiSerial.flush()
+        directSerial.send(bufferOut, bufferOut.size)
+        ftdiSerial.receive(200, bufferIn.size, bufferIn)
+        if (!Arrays.equals(bufferIn, bufferOut)) {
+            waitOnError("[FAIL] Test 2 : Com from Serial to FTDI went wrong")
+        }
+        Timber.d(" test end")
+    }
 
     @Throws(Exception::class)
     private fun areGpio1and2Ok() {
+        Log.d(TAG, "areGpio1and2Ok")
         gpioPort.setPin1(false)
         gpioPort.setPin2(false)
-        Log.d(tag, "1 et 2 LOW")
-        if (ftdiSerial.getCts() === false) {
+        Timber.d("1 et 2 LOW")
+        if (!ftdiSerial.cts) {
             waitOnError("[FAIL] Test 3 : Output 1 or 2 is HIGH instead of LOW")
         }
         gpioPort.setPin1(true)
         gpioPort.setPin2(false)
-        Thread.sleep(100)
-        Log.d(tag, "1 HIGH")
-        if (ftdiSerial.getCts() === true) {
+        SystemClock.sleep(300)
+        Timber.d("1 HIGH")
+        if (ftdiSerial.cts) {
             waitOnError("[FAIL] Test 3 : Output 1 is LOW instead of HIGH")
         }
         gpioPort.setPin1(false)
         gpioPort.setPin2(true)
-        Thread.sleep(100)
-        Log.d(tag, "2 HIGH")
-        if (ftdiSerial.getCts() === true) {
+        SystemClock.sleep(300)
+        Timber.d("2 HIGH")
+        if (ftdiSerial.cts) {
             waitOnError("[FAIL] Test 3 : Output 2 is LOW instead of HIGH")
         }
-        Log.d(tag, "Test ok")
+        Timber.d("Test ok")
     }
 
-    @get:Throws(Exception::class)
-    private val isInput4OK: Unit
-        private get() {
-            gpioPort.setPin1(false)
-            gpioPort.setPin2(false)
-            ftdiSerial.setRts(false)
-            Thread.sleep(100)
-            //TODO control test ExpansionPort.getPin(4) == 0
-            if (!gpioPort.outPin4) {
-                waitOnError("[FAIL] Test 4 : Pin 4 is LOW instead of HIGH")
-            }
-            ftdiSerial.setRts(true)
-            Thread.sleep(100)
-            //TODO control test ExpansionPort.getPin(4) == 1
-            if (gpioPort.outPin4) {
-                waitOnError("[FAIL] Test 4 : Pin 4 is HIGH instead of LOW")
-            }
+    @Throws(Exception::class)
+    private fun isInput4OK() {
+        Log.d(TAG, "isInput4OK")
+        gpioPort.setPin1(false)
+        gpioPort.setPin2(false)
+        ftdiSerial.setRts(false)
+        SystemClock.sleep(200)
+        if (!gpioPort.inPinInput) {
+            waitOnError("[FAIL] Test 4 : Pin 4 is LOW instead of HIGH")
         }
+        ftdiSerial.setRts(true)
+        SystemClock.sleep(200)
+        if (gpioPort.inPinInput) {
+            waitOnError("[FAIL] Test 4 : Pin 4 is HIGH instead of LOW")
+        }
+    }
 
     private fun populateBuffer(buf: ByteArray) {
+        Log.d(TAG, "populateBuffer")
         val ran = Random()
         ran.nextBytes(buf)
     }
